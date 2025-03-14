592a574f205038d624a399a09a0e07040ad12bac
lroussel <lroussel@student.42perpignan.fr>
2025-03-14 18:14:35 +0100

    secure ft_strlen_utf8

--- ft_strlen_utf8.c
+++ ft_strlen_utf8.c
@@ -6,7 +6,7 @@
 /*   By: lroussel <lroussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 16:46:31 by lroussel          #+#    #+#             */
-/*   Updated: 2025/03/14 16:52:37 by lroussel         ###   ########.fr       */
+/*   Updated: 2025/03/14 17:53:02 by lroussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,21 @@
 size_t	ft_strlen_utf8(const char *s)
 {
 	int	i;
+	int	j;
 	int	len;
 
 	i = 0;
 	len = 0;
 	while (s[i])
 	{
-		i += ft_bytes_count(s[i]);
+		j = ft_bytes_count(s[i]);
+		while (j != 0)
+		{
+			if (!s[i])
+				return (len);
+			i++;
+			j--;
+		}
 		len++;
 	}
 	return (len);
