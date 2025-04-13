e51363cb524ded9f3a1e4168f38e909b0af66a7b
lroussel <lroussel@student.42perpignan.fr>
2025-04-13 22:38:56 +0200

    norm

--- ft_isdir.c
+++ ft_isdir.c
@@ -6,18 +6,18 @@
 /*   By: lroussel <lroussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 11:02:47 by lroussel          #+#    #+#             */
-/*   Updated: 2025/04/13 22:36:19 by lroussel         ###   ########.fr       */
+/*   Updated: 2025/04/13 22:38:46 by lroussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <sys/stat.h>
 
-int ft_isdir(const char *path)
+int	ft_isdir(const char *path)
 {
 	struct stat	infos;
 
 	if (stat(path, &infos) == -1)
 		return (0);
-	return (infos.st_mode & S_IFMT) == S_IFDIR;
+	return ((infos.st_mode & S_IFMT) == S_IFDIR);
 }
--- ft_isfile.c
+++ ft_isfile.c
@@ -6,18 +6,18 @@
 /*   By: lroussel <lroussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 22:13:54 by lroussel          #+#    #+#             */
-/*   Updated: 2025/04/13 22:14:51 by lroussel         ###   ########.fr       */
+/*   Updated: 2025/04/13 22:38:26 by lroussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <sys/stat.h>
 
-int ft_isfile(const char *path)
+int	ft_isfile(const char *path)
 {
 	struct stat	infos;
 
 	if (stat(path, &infos) == -1)
 		return (0);
-	return (infos.st_mode & S_IFMT) == S_IFREG;
+	return ((infos.st_mode & S_IFMT) == S_IFREG);
 }
