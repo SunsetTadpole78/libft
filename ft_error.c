5099ee60273a19fc5d290f028df7d1ce4c2d09eb
lroussel <lroussel@student.42perpignan.fr>
2025-05-14 16:17:27 +0200

    fix ft_error method

--- ft_error.c
+++ ft_error.c
@@ -6,15 +6,19 @@
 /*   By: lroussel <lroussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 17:56:14 by lroussel          #+#    #+#             */
-/*   Updated: 2025/05/13 17:57:51 by lroussel         ###   ########.fr       */
+/*   Updated: 2025/05/14 16:15:44 by lroussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_error(char *message, int code)
+int	ft_error(char *message, char *color, int code)
 {
 	if (message)
+	{
+		write(2, color, ft_strlen(color));
 		write(2, message, ft_strlen(message));
+		write(2, "\033[0m", 4);
+	}
 	return (code);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lroussel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 12:35:19 by lroussel          #+#    #+#             */
-/*   Updated: 2025/05/14 16:00:58 by lroussel         ###   ########.fr       */
+/*   Updated: 2025/05/14 16:16:30 by lroussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -195,7 +195,7 @@ t_fvector2		ft_fvector2(float x, float y);
 t_vector3		ft_vector3(int x, int y, int z);
 t_fvector3		ft_fvector3(float x, float y, float z);
 t_rgb			ft_rgb(unsigned int r, unsigned int g, unsigned int b);
-int				ft_error(char *message, int code);
+int				ft_error(char *message, char *color, int code);
 void			ft_free_str_array(char **array);
 int				ft_isfloat(char *s);
 float			ft_atof(char *nptr);
